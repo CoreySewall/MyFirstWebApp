e82d27de4203c67e0a777a2b92d7bb61e97de7a2
Corey Sewall <corey.sewall@toyota.com>
2016-06-02 09:08:47 -0700

    Add project files.

---
+++ MyFirstWebApp/MyJScript.js
@@ -0,0 +1,10 @@
+﻿function calc(num1, num2) {
+    return num1 + num2;
+}
+
+function MyCustomFunction(inNum1, inNum2) {
+    var num1 = inNum1;
+    var num2 = inNum2;
+
+    return (num1 * 10) + num2;
+}
